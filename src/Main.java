206ffff0fccc98890082cb86fa6174b2a2807e0e
emrekabakci <221238040@ogrenci.karatay.edu.tr>
2022-12-18 06:37:46 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int remain, birthYear;
+        String zodiac = "";
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Please enter the year of birth : ");
+        birthYear = inp.nextInt();
+
+        remain = birthYear %12;
+
+        switch (remain) {
+            case 0:
+                zodiac = "Monkey";
+                break;
+            case 1:
+                zodiac = "Rooster";
+                break;
+            case 2:
+                zodiac = "Dog";
+                break;
+            case 3:
+                zodiac = "Pig";
+                break;
+            case 4:
+                zodiac = "Rat";
+                break;
+            case 5:
+                zodiac = "Ox";
+                break;
+            case 6:
+                zodiac = "Tiger";
+                break;
+            case 7:
+                zodiac = "Rabbit";
+                break;
+            case 8:
+                zodiac = "Dragon";
+                break;
+            case 9:
+                zodiac = "Snake";
+                break;
+            case 10:
+                zodiac = "Horse";
+                break;
+            case 11:
+                zodiac = "Goat";
+                break;
+        }
+        if (remain < 0){
+
+            System.out.println("Please check your year of birth");
+
+        }else {
+
+            System.out.println("Your chinese zodiac is : " + zodiac);
+
+        }
+    }
+}
