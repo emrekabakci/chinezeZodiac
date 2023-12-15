032c4778320be4f5c89209176c542502b9a3de0d
emrekabakci <emrekabakci.2@icloud.com>
2023-12-15 22:23:14 +0300

    Comment added.

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //Variables defined
         int remain, birthYear;
         String zodiac = "";
+
+        //New scanner created.
         Scanner inp = new Scanner(System.in);
 
+        //Data retrieved from user.
         System.out.print("Please enter the year of birth : ");
         birthYear = inp.nextInt();
 
+        //Calculation for chinese zodiac.
         remain = birthYear %12;
 
         switch (remain) {
@@ -49,12 +54,13 @@ public class Main {
                 zodiac = "Goat";
                 break;
         }
+        //If remain is less than 0 it gives error.
         if (remain < 0){
 
             System.out.println("Please check your year of birth");
 
         }else {
-
+            //Result printed.
             System.out.println("Your chinese zodiac is : " + zodiac);
 
         }
